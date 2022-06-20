a4c5a29ae3fd5be043c5ed2c2cbd10e59782c982
Jhon Francisco Jove Obando <83244035+Franjo1018@users.noreply.github.com>
2022-06-20 15:47:09 -0500

    Pruebas

--- FormPrincipal.Designer.cs
+++ FormPrincipal.Designer.cs
@@ -29,13 +29,24 @@ namespace ProyectoMOANSO
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(637, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "FormPrincipal";
             this.Text = "Produccion de Muebles";
             this.ResumeLayout(false);
@@ -43,6 +54,8 @@ namespace ProyectoMOANSO
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
