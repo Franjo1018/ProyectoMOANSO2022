27cdc3e2fd5b067b424d9db34129a1039063c184
Jhon Francisco Jove Obando <83244035+Franjo1018@users.noreply.github.com>
2022-06-20 21:18:31 -0500

    ConexionSQL

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CapaDatos
 {
     public class Conexion
     {
+        //Patron Singleton
+        // Variable estática para la instancia
+        private static readonly Conexion _instancia = new Conexion();
+        //privado para evitar la instanciación directa
+        public static Conexion Instancia
+        {
+            get
+            {
+                return Conexion._instancia;
+            }
+        }
+
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=DESKTOP-CVMHKR2; Initial Catalog = MOANSO-Proyecto; Integrated Security = True";
+
+            return cn;
+        }
     }
 }
